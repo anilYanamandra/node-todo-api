a1ec41b5f52ae02bd6403ca3d2c8b8aa2e80f2b3
I329810 <anil.kumar.yanamandra@sap.com>
2018-12-04 17:33:59 +0530

    adding get todos

--- server/server.js
+++ server/server.js
@@ -21,6 +21,15 @@ app.post('/todos', (req, res) => {
   });
 });
 
+
+app.get('/todos', (req,res) =>{
+    Todo.find().then((todos) => {
+        var status = "Success";
+        res.send({status,todos});
+    }, (e) => { res.status(500).send({error: 'Unable to fetch data'})})
+
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
