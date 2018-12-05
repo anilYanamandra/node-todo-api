346d25d213a94606757a6f7c3a0b32fb9a0928af
I329810 <anil.kumar.yanamandra@sap.com>
2018-12-05 14:54:16 +0530

    heroku mongo changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
+var mongodburi = Process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(mongodburi);
 
 module.exports = {mongoose};
