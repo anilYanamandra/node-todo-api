eca82121ae6486b2f22fa95d34ad1cef0a8206eb
I329810 <anil.kumar.yanamandra@sap.com>
2018-12-05 14:58:30 +0530

    fix

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
-var PORT = Process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
